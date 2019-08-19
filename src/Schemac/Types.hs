7a73f1536edf49ec6a6612fa0bc1cc9566ffaac7
Jamie Bertram <jamie@genm.co>
2019-08-19 12:28:20 -0400

    Refactor types

--- src/Schemac/Types.hs
+++ src/Schemac/Types.hs
@@ -1,59 +1,121 @@
 module Schemac.Types where
 
-newtype Prim = Prim { unPrim :: String } deriving (Show, Eq, Ord)
-newtype Tag = Tag { unTag :: String } deriving (Show, Eq, Ord)
+import GHC.Generics
+import Data.Hashable
+
+data Prim = Prim
+    { primId :: Int
+    , primName :: String
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Prim
+
+data Tag = Tag
+    { tagId :: Int
+    , tagName :: String
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Tag
 
 data Case = Case
-    { caseLinks :: [Link]
+    { caseId :: Int
+    , caseLinks :: [Link]
     , caseName :: String
     , caseProps :: [Prop]
-    } deriving (Show, Eq, Ord)
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Case
 
-newtype Data = Data { unData :: [Case] } deriving (Show, Eq, Ord)
+data Data = Data
+    { dataId :: Int
+    , dataName :: String
+    , dataCases :: [Case]
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Data
 
 data Type
     = PrimType Prim
     | DataType Data
-    deriving (Show, Eq, Ord)
-
-data Modifiers
-    = None
-    | Option
-    | Many
-    | ManyOption
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, Generic)
+instance Hashable Type
 
 data Trait = Trait
-    { traitLinks :: [Link]
+    { traitId :: Int
+    , traitLinks :: [Link]
     , traitName :: String
     , traitParents :: [Trait]
     , traitProps :: [Prop]
-    } deriving (Show, Eq, Ord)
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Trait
 
 data Entity = Entity
-    { entityName :: String
+    { entityId :: Int
+    , entityName :: String
     , entityLinks :: [Link]
     , entityProps :: [Prop]
     , entityTraits :: [Trait]
-    } deriving (Show, Eq, Ord)
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Entity
 
 data Link = Link
-    { linkEntity :: Entity
+    { linkId :: Int
+    , linkEntity :: Entity
     , linkName :: String
-    , linkModifiers :: Modifiers
     , linkTags :: [Tag]
-    } deriving (Show, Eq, Ord)
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Link
 
 data Prop = Prop
-    { propName :: Entity
-    , propModifiers :: Modifiers
+    { propId :: Int
+    , propName :: String
     , propTags :: [Tag]
     , propType :: Type
-    } deriving (Show, Eq, Ord)
+    } deriving (Show, Eq, Ord, Generic)
+instance Hashable Prop
 
 data Schema = Schema
-    { schemaPrims :: [Prim]
-    , schemaRoot :: Entity
+    { schemaId :: Int
+    , schemaDatas :: [Data]
+    , schemaEntities :: [Entity]
+    , schemaName :: String
+    , schemaPrims :: [Prim]
     , schemaTags :: [Tag]
     , schemaTraits :: [Trait]
     } deriving (Show, Eq)
+    
+data Emit
+    = CaseLink Int Int
+    | CaseName Int String
+    | CaseProp Int Int
+    | DataCase Int Int
+    | DataName Int String
+    | DecCase Int
+    | DecData Int
+    | DecEntity Int
+    | DecLink Int
+    | DecPrim Int
+    | DecProp Int
+    | DecSchema Int
+    | DecTag Int
+    | DecTrait Int
+    | EntityLink Int Int
+    | EntityName Int String
+    | EntityProp Int Int
+    | EntityTrait Int Int
+    | LinkEntity Int Int
+    | LinkTag Int Int
+    | LinkName Int String
+    | PrimName Int String
+    | PropData Int Int
+    | PropName Int String
+    | PropPrim Int Int
+    | PropTag Int Int
+    | SchemaData Int Int
+    | SchemaEntity Int Int
+    | SchemaName Int String
+    | SchemaPrim Int Int
+    | SchemaTag Int Int
+    | SchemaTrait Int Int
+    | TagName Int String
+    | TraitLink Int Int
+    | TraitName Int String
+    | TraitParent Int Int
+    | TraitProp Int Int
+    deriving (Eq, Show)
